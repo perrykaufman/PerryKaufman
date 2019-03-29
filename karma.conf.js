2ff023fb537a983408c5ecdeb5312b80fa159c17
Perry <19218868+perrykaufman@users.noreply.github.com>
2019-03-29 18:45:24 -0500

    consolodated webpack/karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,52 @@
 /* eslint-env node */
 
-const webpackConfig = require("./webpack.config.js");
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+const webpackConfig = {
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {}
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["env"]
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]"
+        }
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()],
+  resolve: {
+    alias: {
+      Layout: path.resolve(__dirname, "src/.vuepress/theme/Layout.vue"),
+      Lib: path.resolve(__dirname, "src/.vuepress/theme/lib"),
+      Src: path.resolve(__dirname, "src/"),
+      Test: path.resolve(__dirname, "test/"),
+      Theme: path.resolve(__dirname, "src/.vuepress/theme/")
+    },
+    extensions: [".js", ".vue"]
+  }
+};
 
 module.exports = function(config) {
   config.set({
